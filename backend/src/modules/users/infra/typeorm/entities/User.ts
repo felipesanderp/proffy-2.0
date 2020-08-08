ec5b6d5e7e154f694f3f6586e826a289bde786d8
felipesanderp <felipe.panisson@hotmail.com>
2020-08-07 22:05:06 -0300

    add users table

---
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Exclude } from 'class-transformer';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  whatsapp: string;
+
+  @Column()
+  bio: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
