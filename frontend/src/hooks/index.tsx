f866512a14e95d42a44710d8052e2000a17d3431
felipesanderp <felipe.panisson@hotmail.com>
2020-08-12 19:58:07 -0300

    add success page hook

--- frontend/src/hooks/index.tsx
+++ frontend/src/hooks/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { AuthProvider } from './auth';
 import { ToastProvider } from './toast';
+import { SuccessProvider } from './success';
 
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>
-    <ToastProvider>{children}</ToastProvider>
+    <ToastProvider>
+      <SuccessProvider>{children}</SuccessProvider>
+    </ToastProvider>
   </AuthProvider>
 );
 
